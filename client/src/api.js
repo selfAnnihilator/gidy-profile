0d1b2cd0748ff93b1a837a63ab78af035a68388c
selfAnnihilator <159141717+selfAnnihilator@users.noreply.github.com>
2026-02-16 18:55:57 +0530

    Update API base URL import to use VITE_API_BASE_URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BASE_URL } from "./config/api";
+import { VITE_API_BASE_URL } from "./config/api";
 
 export const API = axios.create({
   baseURL: API_BASE_URL
